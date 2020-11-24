c370cf0d6b950497f3a08f013a71e553a9c8ca44
mzhirko <mzhirko@gmail.com>
2020-11-24 20:11:47 +0300

    Update reuirements

--- parking.py
+++ parking.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import cv2
 import mrcnn.config
@@ -6,101 +7,101 @@ from mrcnn.model import MaskRCNN
 from pathlib import Path
 
 
-# Конфигурация, которую будет использовать библиотека Mask-RCNN.
+# Configuration that will be used by the Mask-RCNN library
 class MaskRCNNConfig(mrcnn.config.Config):
     NAME = "coco_pretrained_model_config"
     IMAGES_PER_GPU = 1
     GPU_COUNT = 1
-    NUM_CLASSES = 1 + 80  # в датасете COCO находится 80 классов + 1 фоновый класс.
-    DETECTION_MIN_CONFIDENCE = 0.6
+    NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
+    DETECTION_MIN_CONFIDENCE = 0.2
 
 
-# Фильтруем список результатов распознавания, чтобы остались только автомобили.
+# Filter a list of Mask R-CNN detection results to get only the detected cars / trucks
 def get_car_boxes(boxes, class_ids):
     car_boxes = []
 
     for i, box in enumerate(boxes):
-        # Если найденный объект не автомобиль, то пропускаем его.
+        # If the detected object isn't a car / truck, skip it
         if class_ids[i] in [3, 8, 6]:
             car_boxes.append(box)
 
     return np.array(car_boxes)
 
 
-# Корневая директория проекта.
+# Root directory of the project
 ROOT_DIR = Path(".")
 
-# Директория для сохранения логов и обученной модели.
-MODEL_DIR = ROOT_DIR / "logs"
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
-# Локальный путь к файлу с обученными весами.
-COCO_MODEL_PATH = ROOT_DIR / "mask_rcnn_coco.h5"
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
-# Загружаем датасет COCO при необходимости.
-if not COCO_MODEL_PATH.exists():
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
     mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
 
-# Директория с изображениями для обработки.
-IMAGE_DIR = ROOT_DIR / "images"
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
-# Видеофайл или камера для обработки — вставьте значение 0, если нужно использовать камеру, а не видеофайл.
+# Video file or camera to process - set this to 0 to use your webcam instead of a video file
 VIDEO_SOURCE = "test_images/VID_20201106_170820.mp4"
 
-# Создаём модель Mask-RCNN в режиме вывода.
+# Create a Mask-RCNN model in inference mode
 model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())
 
-# Загружаем предобученную модель.
+# Load pre-trained model
 model.load_weights(COCO_MODEL_PATH, by_name=True)
 
-# Местоположение парковочных мест.
+# Location of parking spaces
 parked_car_boxes = None
 
-# Загружаем видеофайл, для которого хотим запустить распознавание.
+# Load the video file we want to run detection on
 video_capture = cv2.VideoCapture(VIDEO_SOURCE)
 
-# Проходимся в цикле по каждому кадру.
+# Loop over each frame of video
 while video_capture.isOpened():
     success, frame = video_capture.read()
     if not success:
         break
 
-    # Конвертируем изображение из цветовой модели BGR (используется OpenCV) в RGB.
+    # Convert the image from BGR color (which OpenCV uses) to RGB color
     rgb_image = frame[:, :, ::-1]
 
-    # Подаём изображение модели Mask R-CNN для получения результата.
+    # Run the image through the Mask R-CNN model to get results.
     results = model.detect([rgb_image], verbose=0)
 
-    # Mask R-CNN предполагает, что мы распознаём объекты на множественных изображениях.
-    # Мы передали только одно изображение, поэтому извлекаем только первый результат.
+    # Mask R-CNN assumes we are running detection on multiple images.
+    # We only passed in one image to detect, so only grab the first result.
     r = results[0]
 
-    # Переменная r теперь содержит результаты распознавания:
-    # - r['rois'] — ограничивающая рамка для каждого распознанного объекта;
-    # - r['class_ids'] — идентификатор (тип) объекта;
-    # - r['scores'] — степень уверенности;
-    # - r['masks'] — маски объектов (что даёт вам их контур).
+    # The r variable will now have the results of detection:
+    # - r['rois'] are the bounding box of each detected object
+    # - r['class_ids'] are the class id (type) of each detected object
+    # - r['scores'] are the confidence scores for each detection
+    # - r['masks'] are the object masks for each detected object (which gives you the object outline)
 
-    # Фильтруем результат для получения рамок автомобилей.
+    # Filter the results to only grab the car / truck bounding boxes
     car_boxes = get_car_boxes(r['rois'], r['class_ids'])
 
     print("Cars found in frame of video:")
 
-    # Отображаем каждую рамку на кадре.
+    # Draw each box on the frame
     for box in car_boxes:
-        print("Car:", box)
+        print("Car: ", box)
 
         y1, x1, y2, x2 = box
 
-        # Рисуем рамку.
+        # Draw the box
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-    # Показываем кадр на экране.
+    # Show the frame of video on the screen
     cv2.imshow('Video', frame)
 
-    # Нажмите 'q', чтобы выйти.
+    # Hit 'q' to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Очищаем всё после завершения.
+# Clean up everything when finished
 video_capture.release()
 cv2.destroyAllWindows()

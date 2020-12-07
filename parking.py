892a1f7665e30b4553787d49f612a497c3dc25da
mzhirko <mzhirko@gmail.com>
2020-12-07 07:38:29 +0300

    Soo, this works but takes too much time to process video

--- parking.py
+++ parking.py
@@ -7,6 +7,7 @@ from mrcnn.model import MaskRCNN
 from pathlib import Path
 
 
+
 # Configuration that will be used by the Mask-RCNN library
 class MaskRCNNConfig(mrcnn.config.Config):
     NAME = "coco_pretrained_model_config"
@@ -27,7 +28,6 @@ def get_car_boxes(boxes, class_ids):
 
     return np.array(car_boxes)
 
-
 # Root directory of the project
 ROOT_DIR = Path(".")
 
@@ -45,7 +45,7 @@ if not os.path.exists(COCO_MODEL_PATH):
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
 # Video file or camera to process - set this to 0 to use your webcam instead of a video file
-VIDEO_SOURCE = "test_images/VID_20201106_170820.mp4"
+VIDEO_SOURCE = os.path.join(ROOT_DIR, "test-images/parking_video.mp4")
 
 # Create a Mask-RCNN model in inference mode
 model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())
@@ -58,6 +58,12 @@ parked_car_boxes = None
 
 # Load the video file we want to run detection on
 video_capture = cv2.VideoCapture(VIDEO_SOURCE)
+
+# How many frames of video we've seen in a row with a parking space open
+free_space_frames = 0
+
+# Have we sent an SMS alert yet?
+# sms_sent = False
 
 # Loop over each frame of video
 while video_capture.isOpened():
@@ -81,27 +87,69 @@ while video_capture.isOpened():
     # - r['scores'] are the confidence scores for each detection
     # - r['masks'] are the object masks for each detected object (which gives you the object outline)
 
-    # Filter the results to only grab the car / truck bounding boxes
-    car_boxes = get_car_boxes(r['rois'], r['class_ids'])
+    if parked_car_boxes is None:
+        # This is the first frame of video - assume all the cars detected are in parking spaces.
+        # Save the location of each car as a parking space box and go to the next frame of video.
+        parked_car_boxes = get_car_boxes(r['rois'], r['class_ids'])
+    else:
+        # We already know where the parking spaces are. Check if any are currently unoccupied.
 
-    print("Cars found in frame of video:")
+        # Get where cars are currently located in the frame
+        car_boxes = get_car_boxes(r['rois'], r['class_ids'])
 
-    # Draw each box on the frame
-    for box in car_boxes:
-        print("Car: ", box)
+        # See how much those cars overlap with the known parking spaces
+        overlaps = mrcnn.utils.compute_overlaps(parked_car_boxes, car_boxes)
 
-        y1, x1, y2, x2 = box
+        # Assume no spaces are free until we find one that is free
+        free_space = False
 
-        # Draw the box
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        # Loop through each known parking space box
+        for parking_area, overlap_areas in zip(parked_car_boxes, overlaps):
 
-    # Show the frame of video on the screen
-    cv2.imshow('Video', frame)
+            # For this parking space, find the max amount it was covered by any
+            # car that was detected in our image (doesn't really matter which car)
+            max_IoU_overlap = np.max(overlap_areas)
 
+            # Get the top-left and bottom-right coordinates of the parking area
+            y1, x1, y2, x2 = parking_area
+
+            # Check if the parking space is occupied by seeing if any car overlaps
+            # it by more than 0.15 using IoU
+            if max_IoU_overlap < 0.15:
+                # Parking space not occupied! Draw a green box around it
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                # Flag that we have seen at least one open space
+                free_space = True
+            else:
+                # Parking space is still occupied - draw a red box around it
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 1)
+
+            # Write the IoU measurement inside the box
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, f"{max_IoU_overlap:0.2}", (x1 + 6, y2 - 6), font, 0.3, (255, 255, 255))
+
+        # If at least one space was free, start counting frames
+        # This is so we don't alert based on one frame of a spot being open.
+        # This helps prevent the script triggered on one bad detection.
+        if free_space:
+            free_space_frames += 1
+        else:
+            # If no spots are free, reset the count
+            free_space_frames = 0
+
+        # If a space has been free for several frames, we are pretty sure it is really free!
+        if free_space_frames > 10:
+            # Write SPACE AVAILABLE!! at the top of the screen
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, f"SPACE AVAILABLE!", (10, 150), font, 3.0, (0, 255, 0), 2, cv2.FILLED)
+
+        # Show the frame of video on the screen
+        cv2.imshow('Video', frame)
+        cv2.waitKey(0)
     # Hit 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
 # Clean up everything when finished
-video_capture.release()
-cv2.destroyAllWindows()
+# video_capture.release()
+# cv2.destroyAllWindows()
